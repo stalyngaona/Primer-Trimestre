08a5b8f3b60a06a1bc2647b8814e14a6ce0ebd09
stalyngaona <stalyngaona29@gmail.com>
2025-11-23 23:11:14 -0500

    Add files via upload

---
+++ resept6.py
@@ -0,0 +1,42 @@
+import network
+import socket
+import time
+
+# Configurar Access Point
+SSID = "Stalyn Gaona"
+PASSWORD = "Stalyn01"
+
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid=SSID, password=PASSWORD, authmode=network.AUTH_WPA_WPA2_PSK)
+
+# IP fija del AP
+ap.ifconfig(("192.168.4.1", "255.255.255.0", "192.168.4.1", "8.8.8.8"))
+
+print("\nESP32 configurado como Access Point")
+print("SSID:", SSID)
+print("IP:", ap.ifconfig()[0])
+print("\nEsperando datos de sensores...\n")
+
+# Crear socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("192.168.4.1", 5005))
+
+while True:
+    try:
+        data, addr = sock.recvfrom(1024)
+        mensaje = data.decode()
+
+        # Detecta automáticamente cualquier sensor que envíe mensaje con "sensorX:"
+        if mensaje.startswith("sensor"):
+            sensor, valores = mensaje.split(":")
+            temp, hum = valores.split(",")
+            print(f"[{sensor}]  Temp: {temp}°C | Hum: {hum}%   → desde {addr[0]}")
+        else:
+            print("Dato recibido:", mensaje)
+
+    except Exception as e:
+        print("Error al recibir:", e)
+        time.sleep(1)
+        
+        
